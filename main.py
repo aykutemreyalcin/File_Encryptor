5d826f6679d8d0b87cc3829ba7c36b55be11651a
aykutemreyalcin <aykutemyeyalcinn@gmail.com>
2025-03-17 16:30:19 +0100

    adding 'encrypt' and 'decrypt' functions

--- main.py
+++ main.py
@@ -9,3 +9,20 @@ def load_key():
     with open("secret.key", "rb") as key_file:
         return key_file.read()
 
+def encrypt_file(filename, key):
+    with open(filename, "rb") as file:
+        file_data = file.read()
+    f = Fernet(key)
+    encrypted_data = f.encrypt(file_data)
+    with open(filename + ".enc", "wb") as file:
+        file.write(encrypted_data)
+    print("File encrypted successfully!")
+
+def decrypt_file(filename, key):
+    with open(filename, "rb") as file:
+        encrypted_data = file.read()
+    f = Fernet(key)
+    decrypted_data = f.decrypt(encrypted_data)
+    with open(filename.replace(".enc", ""), "wb") as file:
+        file.write(decrypted_data)
+    print("File decrypted successfully!")
